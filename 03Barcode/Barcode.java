1cffdc65dd479893d1b483845da79f601927ec41
Arpita Nag <anag@stuy.edu>
2016-12-08 12:23:43 -0500

    Finish Barcode.java (not tested completely)

--- 03Barcode/Barcode.java
+++ 03Barcode/Barcode.java
@@ -8,8 +8,18 @@ public class Barcode implements Comparable<Barcode>{
 //postcondition: throws a runtime exception zip is not the correct length
 //               or zip contains a non digit
 //               _zip and _checkDigit are initialized.
+    
+    public String getZip(){
+        return _zip;
+        
+    }
+    
+    public int getCheck(){
+           return _checkDigit;
+    }
+        
   public Barcode(String zip) {
-        if(!(allDigits) || zip.length() != 5){
+        if(!(allDigits(zip)) || zip.length() != 5){
             throw new RuntimeException("zip is not the correct length or zip contains a non digit");
         }
         else {
@@ -19,7 +29,7 @@ public class Barcode implements Comparable<Barcode>{
   }
   
   private boolean allDigits(String zipcode){
-       for(i = 0; i < zipcode.length(); i++){
+       for(int i = 0; i < zipcode.length(); i++){
             if (zipcode.charAt(i) < '0' || zipcode.charAt(i) >= '0' + 9) return false;
        }
        return true;
@@ -34,8 +44,8 @@ public class Barcode implements Comparable<Barcode>{
 // postcondition: computes and returns the check sum for _zip
   private int checkSum(){
     int sum = 0;
-        for(i = 0; i < _zip.length(); i++){
-            String s = "" + zip.charAt(i);
+        for(int i = 0; i < _zip.length(); i++){
+            String s = "" + _zip.charAt(i);
             sum += Integer.parseInt(s);
         }
     return sum;
@@ -44,40 +54,51 @@ public class Barcode implements Comparable<Barcode>{
 //postcondition: format zip + check digit + Barcode 
 //ex. "084518  |||:::|::|::|::|:|:|::::|||::|:|"      
   public String toString(){
-    switch (month) {
-            case 0:  zipString = "January";
+    String stringy = _zip + _checkDigit();
+    String stringversion = stringy + "   ";
+      
+      for(int i = 0; i < _zip.length(); i++){
+            String s = "" + _zip.charAt(i);
+            digit = Integer.parseInt(s);
+            String zipString;
+          
+            switch (digit) {
+            
+            case 0:  zipString = "||:::";
                      break;
-            case 2:  zipString = "February";
+             case 1: zipString = ":::||";
                      break;
-            case 3:  zipString = "March";
+            case 2:  zipString = "::|:|";
                      break;
-            case 4:  zipString = "April";
+            case 3:  zipString = "::||:";
                      break;
-            case 5:  monthString = "May";
+            case 4:  zipString = ":|::|";
                      break;
-            case 6:  monthString = "June";
+            case 5:  zipString = ":|:|:";
                      break;
-            case 7:  monthString = "July";
+            case 6:  zipString = " :||::";
                      break;
-            case 8:  monthString = "August";
+            case 7:  zipString = "|:::|";
                      break;
-            case 9:  monthString = "September";
+            case 8:  zipString = "|::|:";
                      break;
-            case 10: monthString = "October";
+            case 9:  zipString = "|:|::";
                      break;
-            case 11: monthString = "November";
-                     break;
-            case 12: monthString = "December";
-                     break;
-            default: monthString = "Invalid month";
+            default: zipString = "Invalid digit";
                      break;
         }
-  
-  
+            stringversion += zipString;
+      }
+      return stringversion;
   }
 
 
 // postcondition: compares the zip + checkdigit, in numerical order. 
   public int compareTo(Barcode other){}
+    int i = Integer.parseInt(getZip()) * 10 +getCheck();
+    int y = Integer.parseInt(other.getZip()) * 10 + other.getCheck();
     
+    Integer a = new Integer(i); Integer b = new Integer(y);
+    
+    return a.compareTo(b);
 }
